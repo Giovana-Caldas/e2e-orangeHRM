d691ecba9bd955a56fc44ec281e3e9ec904752a0
Giovana-Caldas <gcaldas16a@gmail.com>
2026-03-09 19:17:29 -0300

    Base Url added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,7 +13,7 @@ describe('Orange HRM Test', () => {
   }
 
   it('Login - Success', () => { //Login com sucesso
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click() 
@@ -21,7 +21,7 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.dashboradGrid)
   })
   it('Login - Fail', () => { //Login sem sucesso
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') 
+    cy.visit('/auth/login') 
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password) 
     cy.get(selectorsList.loginButton).click()
